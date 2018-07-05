7e4c9aa1ee6b6e657b8bdccbfe04cbe5a4eb9383
buraksaruhan <h.buraksaruhan@gmail.com>
2018-07-05 12:12:21 +0300

    Vanilla v2
    Form basics are done. Need to comprehend.

--- MoneyRelations/admin.py
+++ MoneyRelations/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Expense
 
+class ExpenseAdmin(admin.ModelAdmin):
+   list_display = ('PayerName_text','DateOfDocument','PaymentDate')
+
+
 admin.site.register(Expense)
 # Register your adminmodels here.
--- MoneyRelations/models.py
+++ MoneyRelations/models.py
@@ -1,15 +1,22 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
+from datetime import datetime
+
 
 
 class Expense(models.Model):
-    CustomerName_text=models.CharField('Name of the Customer',max_length=200, help_text="Please enter the name of the Customer")
-    FirmName_text = models.CharField('Name of the Firm', max_length=200, help_text="Please enter the name of the Firm")
+    PayerName_text = models.CharField('Payer Person/Firm',max_length=200, help_text="Please enter the name of the Payer",blank=True)
+    TypeOfDocument = models.CharField('Type of the Document', max_length=200, help_text="Please enter the type of the Document", blank=True)
+    DateOfDocument = models.DateTimeField('The date of the Document', default=datetime.now, blank=True)
+    PaymentStatus = models.BooleanField('Payment Status', default=True)
+    PaymentDate= models.DateField('Date of Payment', default=datetime.now, blank=True )
+    MainCategory = models.CharField('Payer Person/Firm',max_length=200, help_text="Please enter the name of the Category",blank=True)
+    SubCategory = models.CharField('Payer Person/Firm',max_length=200, help_text="Please enter the name of the Payer",blank=True)
+    PaymentSum = models.DecimalField('Cell Phone Number',decimal_places=0, max_digits=12, default=0, validators=[MaxValueValidator(999999999999)])
     description = models.TextField('Description of the Customer',blank=True,help_text="Write a brief description",max_length=100)
-    CustomerCellNumber=models.DecimalField('Cell Phone Number',decimal_places=0, max_digits=12, default=90, validators=[MaxValueValidator(999999999999)])
-    CustomerWorkNumber=models.DecimalField('Work Phone Number', decimal_places=0, max_digits=12, default=90,validators=[MaxValueValidator(999999999999)])
-    Subscription_date=models.DateTimeField('Start of the Subscription')
-    IsHeStillSub = models.BooleanField('Still Subscribed?',default=False)
+    MethodOfPayment = models.CharField('Payer Person/Firm',max_length=200, help_text="Please enter the name of the Payer",blank=True)
+    PayingEmployee = models.CharField('Payer Person/Firm',max_length=200, help_text="Please enter the name of the Payer",blank=True)
+
     def __str__(self):
         return self.CustomerName_text
 
